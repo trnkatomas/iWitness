fcfe9bc6668adf82297ca0f05018214cf1dbfdbf
Tomáš Trnka <tomas.trnka@avast.com>
2021-09-18 11:37:26 +0200

    added dead simple ws server

---
+++ web/ws_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"<<< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f">>> {greeting}")
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
